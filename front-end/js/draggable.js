55eda1c7ae39761955c957587600be6562fbdd76
dhe077 <69486294+dhe077@users.noreply.github.com>
2022-09-10 14:05:05 +1200

    Added Resizable and Dropzone js
    - added the resize-drag script
    - added the dropzone script
    -> when moving the side nav components they do not overlap the workspace
    - tweaked the html and css layout a bit

--- front-end/js/draggable.js
+++ front-end/js/draggable.js
@@ -46,3 +46,105 @@ function dragMoveListener (event) {
 
 // this function is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
+
+
+// Resizable
+interact('.resize-drag')
+    .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
+
+        listeners: {
+            move(event) {
+                var target = event.target
+                var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+                // update the element's style
+                target.style.width = event.rect.width + 'px'
+                target.style.height = event.rect.height + 'px'
+
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left
+                y += event.deltaRect.top
+
+                target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
+                //target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+            }
+        },
+        modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+                outer: 'parent'
+            }),
+
+            // minimum size
+            interact.modifiers.restrictSize({
+                min: { width: 100, height: 50 }
+            })
+        ],
+    })
+    .draggable({
+        // enable autoScroll
+        autoScroll: true,
+
+        listeners: {
+            // call this function on every dragmove event
+            move: dragMoveListener,
+
+            // call this function on every dragend event
+            end(event) {
+                var textEl = event.target.querySelector('p')
+
+                textEl && (textEl.textContent =
+                    'moved a distance of ' +
+                    (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                        Math.pow(event.pageY - event.y0, 2) | 0))
+                        .toFixed(2) + 'px')
+            }
+        }
+    })
+
+
+// Dropzone
+// enable draggables to be dropped into this
+interact('.dropzone')
+    .dropzone({
+        // only accept elements matching this CSS selector
+        // accept: 'drag-1',
+        // Require a 75% element overlap for a drop to be possible
+        overlap: 1,
+
+        // listen for drop related events:
+
+        ondropactivate: function (event) {
+            // add active dropzone feedback
+            event.target.classList.add('drop-active')
+        },
+        ondragenter: function (event) {
+            var draggableElement = event.relatedTarget
+            var dropzoneElement = event.target
+
+            // feedback the possibility of a drop
+            dropzoneElement.classList.add('drop-target')
+            draggableElement.classList.add('can-drop')
+            draggableElement.textContent = 'Dragged in'
+        },
+        ondragleave: function (event) {
+            // remove the drop feedback style
+            event.target.classList.remove('drop-target')
+            event.relatedTarget.classList.remove('can-drop')
+            event.relatedTarget.textContent = 'Dragged out'
+        },
+        ondrop: function (event) {
+            event.relatedTarget.textContent = 'Dropped'
+        },
+        ondropdeactivate: function (event) {
+            // remove active dropzone feedback
+            event.target.classList.remove('drop-active')
+            event.target.classList.remove('drop-target')
+        }
+    })
